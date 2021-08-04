beb7b5771a583deb094ae853f38542aefda4906e
Branden Butler <bwtbutler@hotmail.com>
2021-08-04 15:09:18 -0500

    Add Github Token support, use if no ssh key set

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,68 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
-mkdir -p ~/.ssh -m 700
-echo $2 > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-#echo "SSH ID:"
-#cat ~/.ssh/id_rsa
+# Exit when any command fails
+set -e
 
-eval "$(ssh-agent)"
+# Keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 
-ssh-add ~/.ssh/id_rsa
-ssh-add -l -E sha256
 
+# Trap right before exiting
+trap 'catch $? $LINENO' EXIT
+catch() {
+  # If last exit code was erroneous
+  if [ "$1" != "0" ]; then
+    # error handling goes here
+    echo "${last_command} command failed with exit code $1."
+  fi
+}
+
+# Setup repo workspace
 mkdir -p repo
 cd repo
-git clone git@github.com:$1 .
-echo Contents
+
+# If SSH key not set, use token
+if [ -z "$2" ]
+then
+
+    #Add extra http header telling Github what our auth token is
+    git config --global http.https://github.com/.extraheader "$3"
+
+    # Clone using HTTPS
+    git clone https://github.com/$1.git .
+else
+    # Not using token, so use SSH instead
+
+    # Setup SSH
+    mkdir -p ~/.ssh -m 700
+    echo $2 > ~/.ssh/id_rsa
+    chmod 600 ~/.ssh/id_rsa
+    #echo "SSH ID:"
+    #cat ~/.ssh/id_rsa
+
+    eval "$(ssh-agent)"
+
+    ssh-add ~/.ssh/id_rsa
+    ssh-add -l -E sha256
+
+    # Clone using SSH
+    git clone git@github.com:$1 .
+fi
+
+# Debug info about repo contents
+echo "Contents:"
 ls -al
+
+# Create build dir outside repo and cd to it
 mkdir -p ../build
 cd ../build
+
+# Build docs, assuming the CMakeLists.txt is using CMinx
 cmake ../repo -DBUILD_DOCS=ON
 make docs
+
+# rsync built docs into repo, show diff for debug
 cd ../repo
 git checkout gh-pages
 rsync -a ../build/docs/html/* ./
-git diff
+git --no-pager diff
